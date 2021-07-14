17f08720e57a67787f069d98818f16225f1d02ed
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-07-15 00:21:47 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,38 @@
-import React,{ useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH = process.env.REACT_APP_UNSPLASH_KEY;
 
-
 function App() {
-  const [word,setWord] = useState('')
-  
-  const handleSearchSubmit = (e) =>{
-    e.preventDefault()
-    console.log(e.target[0].value)
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH}`)
-    .then((res)=>res.json())
-    .then((data)=>{
-      console.log(data)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-    .setWord('');
-  }
-  console.log(UNSPLASH)
+  const [word, setWord] = useState("");
+  const [images,setImages] = useState([]);
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setImages([data,...images])
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .setWord("");
+  };
+  console.log(UNSPLASH);
   return (
     <div className="App">
-     <Header title="Image Gallery" />
-     <Search word={word} setWord={setWord} handleSubmit = {handleSearchSubmit} />
+      <Header title="Image Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
-
 
 export default App;
